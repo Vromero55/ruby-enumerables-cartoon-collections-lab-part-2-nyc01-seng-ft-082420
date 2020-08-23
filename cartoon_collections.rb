91a2a6ffd089323600962ab7617343755a06d2fe
Victor Romero <romeroa.victor@yahoo.com>
2020-08-23 15:34:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,8 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do |element|
         element == valid_calls.each
-        
-      
+
+
        
 
 
